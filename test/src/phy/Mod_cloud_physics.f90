10919b5620f57ea22aadc058658fae4b9e89d9f1
hanjihye9490 <62691764+hanjihye9490@users.noreply.github.com>
2020-04-06 23:44:56 +0900

    change physics program
    프로그램 일부 수정!!

--- test/src/phy/Mod_cloud_physics.f90
+++ test/src/phy/Mod_cloud_physics.f90
@@ -2,16 +2,21 @@ PROGRAM Mod_cloud_physics
 
   IMPLICIT NONE
   INTEGER, PARAMETER :: nz=100, nbin=100
-  REAL, PARAMETER :: rmin= 0.1, rratio = 2.
+  REAL, PARAMETER :: rmin= 0.1, rratio = 1.05 
+  INTEGER, PARAMETER :: size_dist_option = 1
+ !! 1 : log-nomal distribution (using sampledata)
+ !! 2 : log-nomal distribution (using Population)
+ !! 3 : gamma distribution 
   REAL :: rmax
   REAL, DIMENSION(nz, nbin) :: conc
   REAL, DIMENSION(nbin) :: r, m, n_r
   REAL, DIMENSION(nbin+1) :: rb, mb
   INTEGER :: ir
   REAL :: pi
-  REAL :: N0, r0, sigma, dev_r
+  REAL :: N0, r0, sigma
 
   pi = ACOS(-1.)
+!  pi = 4.0*atan(1.0)
 
   rmax = rmin*(rratio**nbin)
 
@@ -32,29 +37,46 @@ PROGRAM Mod_cloud_physics
 
   r = m**(1./3.)   
 
-!print*, '    '
-!print*, m     
-!
-!print*, '    '
-!print*, r     
-!
+!  DO ir = 1, nbin
+!    r(ir) = rmin*(rratio**ir)
+!  ENDDO
 
   !--------------------------------------!
 
-!  r0 = SUM(r)/REAL(nbin)
-!  sigma = SQRT(SUM((r-r0)**2)/REAL(nbin))
-  r0 = -1.
-  sigma = 0.4 
+  SELECT CASE(size_dist_option)
+    CASE(1)
 
-  N0 = 1.
+      !! Using sample value
+      r0 = SUM(r)/REAL(nbin)
+      sigma = SQRT(SUM((r-r0)**2)/REAL(nbin))
+      N0 = 100.
+      DO ir = 1, nbin
+        n_r(ir) = (N0/(SQRT(2*pi)*log(sigma)*r(ir))) *  EXP(-1.*((log(r(ir))-log(r0))**2)/(2*(log(sigma)**2)))
+      ENDDO
 
-  DO ir = 1, nbin
-    n_r(ir) = (N0/(SQRT(2*pi)*sigma*r(ir))) * EXP(((log(r(ir)/r0))**2)/(2.*(sigma**2))*-1.)
-!    n_r(ir) = (N0/(SQRT(2*pi)*log(sigma))) * EXP(((log(r(ir)/r0))**2)/(2*(log(sigma)**2))*-1.)
-    print*, log(r(ir)), n_r(ir)
-  ENDDO
-!  print*, n_r
-!  print*, SUM(n_r * r)
-!  print*, SUM(n_r)
+    CASE(2)
+      !! Using Population value
+      r0 = -1. 
+      sigma = 0.4 
+      N0 = 1.
 
+      DO ir = 1, nbin
+        n_r(ir) = (N0/(SQRT(2*pi)*sigma*r(ir))) * EXP(((log(r(ir))-r0)**2)/(2.*(sigma**2))*-1.)
+      ENDDO
+    CASE(3)
+      !! gamma distribution
+!      N0 = ??
+!      mu = ??
+!      lambda = ??
+!
+!      DO ir = 1, nbin
+!        n_r(ir) = N0*r(ir)**mu*exp(-1.0*lambda*r(ir))
+!      ENDDO
+     CASE DEFAULT
+       PRINT*, 'please select option of size distribution' 
+       PRINT*, '1 : log-nomal distribution (using sampledata)'
+       PRINT*, '2 : log-nomal distribution (using Population)'
+       PRINT*, '3 : gamma distribution                       '
+
+   END SELECT
 END PROGRAM Mod_cloud_physics
